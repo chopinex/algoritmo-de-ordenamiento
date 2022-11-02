0d8d03250dde52b300331bb0ace5a7d8d10ecaaf
chopinex <arnariv@gmail.com>
2022-11-02 09:52:20 -0500

    many tries

--- reactjs/src/components/Main.js
+++ reactjs/src/components/Main.js
@@ -1,4 +1,5 @@
 import Elemento from './Elemento.js';
+import {useState} from "react";
 import '../styles/Main.css';
 
 function shuffle(array) {
@@ -20,17 +21,24 @@ function shuffle(array) {
 }
 
 function Main(props){
+    const itemRefs = useRef([]);
+
     const elems=[];
     const rojo = (227-247)/props.cant;
     const verde = (45-255)/props.cant;
     const azul = 72/props.cant;
     const tam = 72/props.cant;
+    
+    const [swap1,setSwap1]=useState(0);
+    const [swap2,setSwap2]=useState(9);
 
     for(let i=0;i<props.cant;i++){
         elems.push(<Elemento size={18+tam*i + 'px'} color={'rgb(' + String(247+rojo*i) +
-         ',' + String(255+verde*i) + ',' + String(azul*i) + ')'}/>);
+         ',' + String(255+verde*i) + ',' + String(azul*i) + ')'} key={i} />);
     }
+
     shuffle(elems);
+    console.log(elems[i]);
 
     return(
         <div className="central">
